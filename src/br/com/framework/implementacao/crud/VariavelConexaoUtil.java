455df6e1caba28c415d1392291e7a1da9cfe63b5
Diego <diegorpitoco@gmail.com>
2019-10-08 15:46:28 -0300

    Criação dos pacotes br.com.framework.hibernate.session e br.com.framework.implementacao.crud e as classes HibernateUtil e VariavelConexaoUtil

---
+++ src/br/com/framework/implementacao/crud/VariavelConexaoUtil.java
@@ -0,0 +1,11 @@
+package br.com.framework.implementacao.crud;
+/**
+ * Nome do caminho do JNDI datasource Tomcat
+ * @author diegopitocodrp
+ * 
+ * */
+public class VariavelConexaoUtil {
+
+	public static String JAVA_COMP_ENV_JBDC_DATASOURCE = "java:/comp/env/jdbc/datasource";
+	
+}
