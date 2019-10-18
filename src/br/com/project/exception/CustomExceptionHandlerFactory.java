755acc2ef19323629b52900314b641fe4b7ece56
Diego <diegorpitoco@gmail.com>
2019-10-18 15:34:21 -0300

    Configuração spring-servlet-servlet, faces-config, applicationContext, CustomExceptionHandlerFactory e login

--- src/br/com/project/exception/CustomExceptionHandlerFactory.java
+++ src/br/com/project/exception/CustomExceptionHandlerFactory.java
@@ -3,11 +3,11 @@ package br.com.project.exception;
 import javax.faces.context.ExceptionHandler;
 import javax.faces.context.ExceptionHandlerFactory;
 
-public class CustomExceptionHandleFactory extends ExceptionHandlerFactory{
+public class CustomExceptionHandlerFactory extends ExceptionHandlerFactory{
 
 	private ExceptionHandlerFactory parent;
 	
-	public CustomExceptionHandleFactory( ExceptionHandlerFactory parent) {
+	public CustomExceptionHandlerFactory( ExceptionHandlerFactory parent) {
 	 this.parent = parent;
 	}
 
