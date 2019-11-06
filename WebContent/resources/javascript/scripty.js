595feebb32513eeeb5ebbe5a069155755612f542
Diego Pitoco <diegorpitoco@gmail.com>
2019-11-05 23:02:12 -0300

    Término da configuração do template

--- WebContent/resources/javascript/scripty.js
+++ WebContent/resources/javascript/scripty.js
@@ -80,3 +80,59 @@ function redirecionarPagina (context, pagina) {
 	pagina = pagina + ".jsf";
 	document.location = context + pagina;
 }
+
+function localeData_pt_br() {
+	PrimeFaces.locales['pt'] = {
+		closeText : 'Fechar',
+		prevText : 'Anterior',
+		nextText : 'Próximo',
+		currentText : 'Começo',
+		monthNames : [ 'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio',
+				'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro',
+				'Dezembro' ],
+		monthNamesShort : [ 'Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul',
+				'Ago', 'Set', 'Out', 'Nov', 'Dez' ],
+		dayNames : [ 'Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta',
+				'Sexta', 'Sábado' ],
+		dayNamesShort : [ 'Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb' ],
+		dayNamesMin : [ 'D', 'S', 'T', 'Q', 'Q', 'S', 'S' ],
+		weekHeader : 'Semana',
+		firstDay : 0,
+		isRTL : false,
+		showMonthAfterYear : false,
+		yearSuffix : '',
+		timeOnlyTitle : 'São Horas',
+		timeText : 'Tempo',
+		hourText : 'Hora',
+		minuteText : 'Minuto',
+		secondText : 'Segundo',
+		ampm : false,
+		month : 'Mês',
+		week : 'Semana',
+		day : 'Dia',
+		allDayText : 'Todo o Dia'
+	};
+}
+
+
+function initTemplate() {
+	$(document).ready(function() {
+		  $('#menupop').hide();
+		  $('#barramenu').hide();
+		  $('#barramenu').css("left", "-200px");
+		  $('#iniciarmenu').click(function() {
+		  	if ($('#barramenu').is(':visible')) {
+		  	  ocultarMenu();
+		  	} else {
+		  	  $('#barramenu').show();
+		  	  $('#barramenu').animate({"left":"0px"}, "slow");	
+		  	}
+		  });
+		});
+	}
+
+function ocultarMenu() {  
+	  $('#barramenu').animate({"left":"-200px"}, "slow", function() {
+	  	$('#barramenu').hide();
+	  });
+	}
